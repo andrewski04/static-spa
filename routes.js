bf2aee0f9ef4449f0343e7421b7bc65f6816b1d7
andrew <andrew@housermail.com>
2023-09-14 19:48:41 -0400

    pages test

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 const routes = {
-  page1: { path: "/html/pages/page1.html" },
-  page2: { path: "/html/pages/page2.html" },
+  page1: { path: "/static-spa/html/pages/page1.html" },
+  page2: { path: "/static-spa/html/pages/page2.html" },
 };
 
 const components = {
-  sidebar: { path: "/html/components/sidebar.html" },
+  sidebar: { path: "/static-spa/html/components/sidebar.html" },
 };
 
 export { routes, components };
