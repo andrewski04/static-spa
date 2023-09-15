60924b9c5d5274b270db5a20cf9f71d7d83e9185
andrew <andrew@housermail.com>
2023-09-14 20:08:29 -0400

    github page routes

--- routes.js
+++ routes.js
@@ -3,13 +3,13 @@ const indexPage = "page1";
 
 // list of routes to different pages
 const routes = {
-  page1: { path: "/html/pages/page1.html" },
-  page2: { path: "/html/pages/page2.html" },
+  page1: { path: "/static-spa/html/pages/page1.html" },
+  page2: { path: "/static-spa/html/pages/page2.html" },
 };
 
 // list of routes to components
 const components = {
-  header: { path: "/html/components/header.html" },
+  header: { path: "/static-spa/html/components/header.html" },
 };
 
 export { routes, components, indexPage };
